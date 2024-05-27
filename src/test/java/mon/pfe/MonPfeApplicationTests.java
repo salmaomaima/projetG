4119dcc0c584615c9848dc815f20cd7243ad7cac
hibayagoubi1 <hibayaagoubi4@gmail.com>
2024-05-27 05:02:24 +0100

    class test modifiction

--- src/test/java/mon/pfe/MonPfeApplicationTests.java
+++ src/test/java/mon/pfe/MonPfeApplicationTests.java
@@ -1,17 +1,9 @@
 package mon.pfe;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import mon.pfe.MonPfeApplication;
-
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = MonPfeApplication.class)
-@WebAppConfiguration
+@SpringBootTest
 public class MonPfeApplicationTests {
 
 	@Test
